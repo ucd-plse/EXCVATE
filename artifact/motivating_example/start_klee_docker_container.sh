8ef1d16e0873a9a5ffb9cea515beea4225f70918
Jackson Vanover <jacksonvanover@gmail.com>
2025-05-09 09:57:26 -0700

    fixed user permissions in klee docker container

--- artifact/motivating_example/start_klee_docker_container.sh
+++ artifact/motivating_example/start_klee_docker_container.sh
@@ -4,8 +4,8 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 cd ${REPO_ROOT}
 
 if [ -z "$1" ]; then
-    docker run -it --rm -v $(realpath ./submodules/FPGen):/home/fptesting/FPTesting -v $(realpath ./artifact/motivating_example):/home/fptesting/EXCVATE_motivating_example --ulimit='stack=-1:-1' ucdavisplse/fpgen-artifact:icse20
+    docker run -it --rm -u $(id -u):$(id -g) -v $(realpath ./submodules/FPGen):/home/fptesting/FPTesting -v $(realpath ./artifact/motivating_example):/home/fptesting/EXCVATE_motivating_example --ulimit='stack=-1:-1' ucdavisplse/fpgen-artifact:icse20
 else
     command="cd /home/fptesting && source .profile && ${1}"
-    docker run --rm -v $(realpath ./submodules/FPGen):/home/fptesting/FPTesting -v $(realpath ./artifact/motivating_example):/home/fptesting/EXCVATE_motivating_example --ulimit='stack=-1:-1' ucdavisplse/fpgen-artifact:icse20 bash -c "${command}"
+    docker run --rm -u $(id -u):$(id -g) -v $(realpath ./submodules/FPGen):/home/fptesting/FPTesting -v $(realpath ./artifact/motivating_example):/home/fptesting/EXCVATE_motivating_example --ulimit='stack=-1:-1' ucdavisplse/fpgen-artifact:icse20 bash -c "${command}"
 fi
