69f7d5cb9b9e2978bb5e057ae3b758fe646f6e16
Jackson Vanover <jacksonvanover@gmail.com>
2025-12-04 16:57:12 -0800

    updated tutorial

--- tutorial/squared_hinge_losses.f90
+++ tutorial/squared_hinge_losses.f90
@@ -3,14 +3,14 @@ module squared_hinge_losses
 
     contains
 
-        real function squared_hinge_loss1(x)
-            real :: x
-            squared_hinge_loss1 = (max(0.0, 1-x))**2
+        real function squared_hinge_loss1(y, t)
+            real :: y, t
+            squared_hinge_loss1 = (max(0.0, 1-y*t))**2
         end function
 
-        real function squared_hinge_loss2(x)
-            real :: x
-            squared_hinge_loss2 = (max(1-x, 0.0))**2
+        real function squared_hinge_loss2(y, t)
+            real :: y, t
+            squared_hinge_loss2 = (max(1-y*t, 0.0))**2
         end function
 
 end module 
